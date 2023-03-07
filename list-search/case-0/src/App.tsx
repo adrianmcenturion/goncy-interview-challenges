0296901c239d1026dc7182d37db18e96c1a01aaa
adrianmcenturion <adrianmcenturion@gmail.com>
2023-03-06 22:45:04 -0300

    case 0 nivel 1,2,3,extra completados

--- list-search/case-0/src/App.tsx
+++ list-search/case-0/src/App.tsx
@@ -7,23 +7,40 @@ import api from "./api";
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
   const [query, setQuery] = useState<string>("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getProducts = async (query?: string) => {
+    setIsLoading(true);
+    let res = await api.search(query);
+
+    setProducts(res);
+    setIsLoading(false);
+  };
 
   useEffect(() => {
-    api.search(query);
+    getProducts();
+  }, []);
+
+  useEffect(() => {
+    getProducts(query);
   }, [query]);
 
   return (
     <main>
       <h1>Tienda digitaloncy</h1>
-      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target)} />
+      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target.value)} />
       <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <h4>{product.title}</h4>
-            <p>{product.description}</p>
-            <span>$ {product.price}</span>
-          </li>
-        ))}
+        {!isLoading ? (
+          products.map((product) => (
+            <li key={product.id} className={`${product.price <= 100 ? "sale" : ""}`}>
+              <h4>{product.title}</h4>
+              <p>{product.description}</p>
+              <span>$ {product.price}</span>
+            </li>
+          ))
+        ) : (
+          <div className="isLoading">Cargando...</div>
+        )}
       </ul>
     </main>
   );
